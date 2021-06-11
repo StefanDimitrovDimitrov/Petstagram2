28dba82759613d380d296139d279a557422a9f40
Stefan Dimitrov <54206891+StefanDimitrovDimitrov@users.noreply.github.com>
2021-06-11 13:48:37 +0300

    create a pet app

--- Petstagram2/settings.py
+++ Petstagram2/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Petstagram2.common'
+    'Petstagram2.common',
+    'Petstagram2.pets'
 ]
 
 MIDDLEWARE = [
